1fccb5cfe155a2abfc5c7cbc6f5fd2ca93610236
Prahlad Narayan <91488768+Prahlad-Narayan@users.noreply.github.com>
2025-07-31 15:15:14 -0400

    backend API changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://gmap-0emf.onrender.com";
+const API_BASE = "https://bb24cb206845.ngrok-free.app/";
 let map, userPos, directionsService, directionsRenderer;
 let tempLatLng = null;
 let markers = [];
